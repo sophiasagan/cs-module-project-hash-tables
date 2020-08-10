cdd19dc68ed840b5846b3c51df8e591e9c237f03
Sophia Sagan <sophiabraddock82@gmail>
2020-08-10 19:13:08 -0400

    add comments

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,21 +1,25 @@
 def word_count(s):
     # Your code here
+    # init cache/dict
     word_d = {}
-
+    # remove nonalphabetic chars
     remove = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'
     
+    # split words into list
     words = s.split()
 
+    # iterate through the list of words
     for word in words:
-        word = word.strip(remove)
+        # strip invalid chars, lowercase all letters
+        # word = word.strip(remove)
         word = "".join(i.lower() for i in word if not i in remove)
-
+        # check if word is valid
         if word == '': 
             continue
-
+        # check dict for word
         if word not in word_d:
             word_d[word] = 0
-
+        # add to word count
         word_d[word] += 1
 
     return word_d
